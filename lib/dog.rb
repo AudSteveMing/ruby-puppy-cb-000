528b630a42e27b2e9a382a13b2570ff822c306ec
Steve Cordova <cordovas@bc.edu>
2020-08-01 03:05:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Add your code here
+class Dog
+
+  @@all = []
+
+  def initialize(name)
+
+  end
+
+  def self.all
+  end
+
+  def self.clear_all
+  end
+
+  def self.print_all
+  end
+
+  def save
+  end
+  
+
+end
